2d8097f6783c4d8fbf60cf05e482fd47f7313a6f
Aage Barlund <barlund@gmail.com>
2025-01-22 09:14:04 +0100

    small adjustment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <nav className="w-screen h-24 bg-slate-100 dark:bg-slate-800 text-slate-800 dark:text-slate-50 border-b-2 border-gray-300 text-4xl content-center">
+    <nav className="w-screen h-24 bg-slate-200 dark:bg-slate-700 text-slate-800 dark:text-slate-50 border-b-2 border-gray-300 text-4xl content-center">
       <ul className="flex gap-5 justify-center">
         <NavItem title="Home" to="/" />
         <NavItem title="About" to="/about" />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 const Home = () => {
     return(
         <div className="h-48 bg-slate-100 dark:bg-slate-800 text-slate-800 dark:text-slate-50 content-center">
